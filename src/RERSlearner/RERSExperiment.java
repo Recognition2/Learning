657642e22b3b3bafbc17f798597be2a582e798db
Recognition2 <gkh998@gmail.com>
2018-03-21 19:56:01 +0100

    Mooie commit

--- src/RERSlearner/RERSExperiment.java
+++ src/RERSlearner/RERSExperiment.java
@@ -17,11 +17,12 @@ public class RERSExperiment {
      */
     public static void main(String [] args) throws IOException {
         // Load the System Under Learning (SUL)
-        SUL<String,String> sul = new ProcessSUL("/Users/sicco/Dropbox/STR_20162017/ReachabilityRERS2017/Problem10/a.out");
+        SUL<String,String> sul = new ProcessSUL("/home/gregory/git/Learning/Problems/Problem10/Problem10.out");
 
 
         // the input alphabet
         Collection<String> inputAlphabet = ImmutableSet.of("1","2","3","4","5");
+//        Collection<String> inputAlphabet = ImmutableSet.of("1","2","3","4","5","6","7","8","9","10");
 
         try {
             // runControlledExperiment for detailed statistics, runSimpleExperiment for just the result
